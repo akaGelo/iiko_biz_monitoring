55bd529001951603bf10738e609b44cd014ca184
Oleg Vyukov <oleg@cafetoolkit.com>
2020-02-12 11:59:57 +0300

    readme

---
+++ main.py
@@ -0,0 +1,140 @@
+import json
+import time
+import requests
+
+
+def page(login, check_result, token, nomenclature):
+    timestamp = time.time()
+
+    template = """
+<html>
+<head>
+    <meta http-equiv="refresh" content="5" >
+</head>
+
+<body>
+status: {login} <b>{status}</b>
+
+<br/>
+
+token: {token}
+
+<br/>
+
+time: {timestamp}
+<br/>
+nomenclature revision : {revision} 
+
+
+server response: {response}
+
+<br/>
+</body>
+
+</html>
+"""
+    return template.format(login=login, status=check_result['status'], timestamp=timestamp, token=token,
+                           revision=nomenclature['revision'], response=check_result['response'])
+
+
+def get_iikobiz_token(login, password):
+    url = "https://iiko.biz:9900/api/0/auth/access_token?user_id=%s&user_secret=%s" % (login, password)
+    token_response = requests.get(url)
+    token = json.loads(token_response.text)
+    return token
+
+
+def get_nomenclature(token, orgId):
+    url = "https://iiko.biz:9900/api/0/nomenclature/%s?access_token=%s" % (orgId, token)
+    nomenclature_response = requests.get(url)
+    nomenclature = json.loads(nomenclature_response.text)
+    return nomenclature
+
+
+def demo_order(nomenclature, org_id):
+    products = nomenclature['products']
+    print(products)
+
+    product = next(product for product in products if (not product['modifiers'] and not product['groupModifiers']))
+    if not product:
+        raise NameError('No product without modifiers')
+
+    return {
+        "organization": org_id,
+        "customer": {
+            "name": "Иван",
+            "phone": "71235678901"
+        },
+        "order": {
+            "phone": "71235678901",
+            "isSelfService": "false",
+            "items": [
+                {
+                    "id": product['id'],
+                    "name": "Паста с говядиной",
+                    "amount": 1,
+                    "code": product['code'],
+                    "sum": product['price'],
+                }
+            ],
+            "address": {
+                "street": "Ленина",
+                "home": "1",
+                "housing": "",
+                "comment": "Скрипт проверки"
+            }
+        }
+    }
+
+
+def check_status(check_order_response):
+    try:
+        json_response = json.loads(check_order_response.text)
+        if not json_response['problem']:
+            return "success"
+        return "fail"
+    except Exception as err:
+        return "fail"
+
+
+def check_order(nomenclature, token, orgId):
+    url = "https://iiko.biz:9900/api/0/orders/checkCreate?access_token=%s" % (token)
+
+    order = demo_order(nomenclature, orgId)
+
+    check_order_response = requests.post(url, json=order)
+
+    status = check_status(check_order_response)
+    status_code = 200 if 'success'==status else 500
+    return {
+        'status': status,
+        'status_code': status_code,
+        'response': check_order_response.text
+
+    }
+
+
+def handler(event, context):
+    login = ''
+    if 'queryStringParameters' in event and 'login' in event['queryStringParameters']:
+        login = event['queryStringParameters']['login']
+    password = ''
+    if 'queryStringParameters' in event and 'password' in event['queryStringParameters']:
+        password = event['queryStringParameters']['password']
+
+    org_id = ''
+    if 'queryStringParameters' in event and 'org_id' in event['queryStringParameters']:
+        org_id = event['queryStringParameters']['org_id']
+
+    token = get_iikobiz_token(login, password)
+    nomenclature = get_nomenclature(token, org_id)
+    check_result = check_order(nomenclature, token, org_id)
+
+    return {
+        'statusCode': check_result['status_code'],
+        'headers': {
+            'Content-Type': 'text/html'
+        },
+        'isBase64Encoded': False,
+        'body': page(login, check_result, token, nomenclature)
+    }
---
+++ main_tests.py
@@ -0,0 +1,39 @@
+import os
+import unittest
+from datetime import datetime
+
+from main import *
+
+test_iiko_login = os.environ.get('IIKO_LOGIN')
+test_iiko_password = os.environ.get('IIKO_PASSWORD')
+test_iiko_org_id = os.environ.get('IIKO_ORG_ID')
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_get_token(self):
+        token = get_iikobiz_token(test_iiko_login, test_iiko_password)
+        self.assertIsNotNone(token)
+        return token
+
+    def test_get_nomenclature(self):
+        token = self.test_get_token()
+        nomenclature = get_nomenclature(token, test_iiko_org_id)
+        self.assertIsNotNone(nomenclature['revision'])
+        return  nomenclature
+
+    def test_check_order(self):
+        token = self.test_get_token()
+        nomenclature = self.test_get_nomenclature()
+
+        result = check_order(nomenclature, token, test_iiko_org_id)
+
+        self.assertEqual('success',result['status'])
+        self.assertEqual(200,result['status_code'])
+
+
+if __name__ == '__main__':
+    print(test_iiko_login)
+    print(test_iiko_password)
+    print(test_iiko_org_id)
+    unittest.main()
